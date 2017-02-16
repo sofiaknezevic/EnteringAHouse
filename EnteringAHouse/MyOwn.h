d196929e2e59a6c010f7c2e739e84d2f5690c964
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-15 18:10:20 -0800

    All working except navbar button

---
+++ EnteringAHouse/MyOwn.h
@@ -0,0 +1,13 @@
+//
+//  MyOwn.h
+//  EnteringAHouse
+//
+//  Created by Sofia Knezevic on 2017-02-15.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyOwn : UIViewController
+
+@end
---
+++ EnteringAHouse/MyOwn.m
@@ -0,0 +1,35 @@
+//
+//  MyOwn.m
+//  EnteringAHouse
+//
+//  Created by Sofia Knezevic on 2017-02-15.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import "MyOwn.h"
+
+@interface MyOwn ()
+
+@end
+
+@implementation MyOwn
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIBarButtonItem *frontDoor = [[UIBarButtonItem alloc] initWithTitle:@"Go Back To Front Door" style:UIBarButtonItemStylePlain target:self action:@selector(returnToFrontDoor)];
+    self.navigationItem.rightBarButtonItem = frontDoor;
+}
+
+- (void)returnToFrontDoor
+{
+    [self.navigationController popToRootViewControllerAnimated:YES];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+\
+
+@end
